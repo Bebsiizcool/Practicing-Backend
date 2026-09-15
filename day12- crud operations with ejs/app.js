ee43cb27d0bd87c559af44e658b56daba921f858
bebsiizcool <basilshahzad18@gmail.com>
2026-09-15 20:42:28 +0500

    day13

--- day12- crud operations with ejs/app.js
+++ day12- crud operations with ejs/app.js
@@ -14,10 +14,20 @@ app.get('/', (req, res)=>{
     res.render("index")
 })
 
-app.get('/delete/:id', async(req, res)=>{
-      let allusers = await usermodel.findOneAndDelete({_id: req.params.id})
-      res.redirect("/read")
-    
+app.get('/read', async(req, res)=>{
+    let allusers = await usermodel.find()
+    res.render("read", {allusers})
+})
+
+app.get('/edit/:id', async(req, res)=>{
+   let user = await usermodel.findOne({ _id: req.params.id})
+   res.render("edit", {user})
+})
+
+app.post('/update/:id', async(req, res)=>{
+    let {name, email, image} = req.body
+    let user = await usermodel.findOneAndUpdate({ _id: req.params.id}, {name, email, image}, {new:true})
+    res.redirect("/read")
 })
 
 
@@ -33,9 +43,14 @@ app.post('/create', async (req, res)=>{
     res.redirect('/read')
 })
 
-app.get('/read', async(req, res)=>{
-    let allusers = await usermodel.find()
-    res.render("read", {allusers})
+app.get('/delete/:id', async(req, res)=>{
+      let allusers = await usermodel.findOneAndDelete({_id: req.params.id})
+      res.redirect("/read")
+    
 })
+
+
+
+
 
 app.listen(3000)
